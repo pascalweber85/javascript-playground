38d007bc6030de4af178e36d772191490bd48604
Pascal Weber <weber-pascal@web.de>
2021-04-28 14:06:12 +0200

    change js

--- src/index.js
+++ src/index.js
@@ -1,37 +1,14 @@
-console.clear()
-const box1 = document.querySelector('.box1')
-const box2 = document.querySelector('.box2')
-const box3 = document.querySelector('.box3')
-const box4 = document.querySelector('.box4')
-const button1 = document.querySelector('.button1')
-const button2 = document.querySelector('.button2')
-const button3 = document.querySelector('.button3')
-const button4 = document.querySelector('.button4')
+const form = document.querySelector('[data-form]')
+// const header = document.querySelector('[data-header]')//
+const input = document.querySelector('[data-input]')
+const box1 = document.querySelector('[data-box]')
 
-button1.addEventListener('click', () => {
-  box1.classList.remove('hidden')
-  box2.classList.add('hidden')
-  box3.classList.add('hidden')
-  box4.classList.add('hidden')
+form.addEventListener('submit', currywurst => {
+  currywurst.preventDefault()
+  box1.classList.toggle(input.value)
 })
 
-button2.addEventListener('click', () => {
-  box1.classList.add('hidden')
-  box2.classList.remove('hidden')
-  box3.classList.add('hidden')
-  box4.classList.add('hidden')
-})
-
-button3.addEventListener('click', () => {
-  box1.classList.add('hidden')
-  box2.classList.add('hidden')
-  box3.classList.remove('hidden')
-  box4.classList.add('hidden')
-})
-
-button4.addEventListener('click', () => {
-  box1.classList.add('hidden')
-  box2.classList.add('hidden')
-  box3.classList.add('hidden')
-  box4.classList.remove('hidden')
-})
+// form.addEventListener('submit', currywurst => {
+//   currywurst.preventDefault()
+//   header.innerText = input.value
+// })
