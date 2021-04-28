74e9ee91542837f274632231a4d7832404d22939
Pascal Weber <weber-pascal@web.de>
2021-04-28 15:47:55 +0200

    input4

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 const form = document.querySelector('[data-form]')
+const box = document.querySelector('.box')
 const input1 = document.querySelector('[name="input1"]')
-const input2 = document.querySelector('[name="input2"]')
 
 form.addEventListener('submit', event => {
   event.preventDefault()
   const inputText1 = input1.value
-  const inputText2 = input2.value
-  input1.value = inputText2.toUpperCase()
-  input2.value = inputText1.toLowerCase()
+  box.style.background = inputText1
+  form.reset()
+  input1.focus()
 })
