3bb52f35995dfd3c8b44779de5c1a0d8f688ff60
Pascal Weber <weber-pascal@web.de>
2021-04-29 15:15:10 +0200

    implement getElement

--- src/index.js
+++ src/index.js
@@ -1,47 +1,41 @@
-const buttonHome = document.querySelector('.button-home')
-const buttonBookmarks = document.querySelector('.button-bookmarks')
-const buttonCreate = document.querySelector('.button-create')
-const buttonSettings = document.querySelector('.button-settings')
-const homePage = document.querySelector('.page-home')
-const bookmarksPage = document.querySelector('.page-bookmarks')
-const createPage = document.querySelector('.page-create')
-const settingsPage = document.querySelector('.page-settings')
+const buttonHome = getElement('.button-home')
+const buttonBookmarks = getElement('.button-bookmarks')
+const buttonCreate = getElement('.button-create')
+const buttonSettings = getElement('.button-settings')
+
+const homePage = getElement('.page-home')
+const bookmarksPage = getElement('.page-bookmarks')
+const createPage = getElement('.page-create')
+const settingsPage = getElement('.page-settings')
 
 buttonHome.addEventListener('click', navigateToHome)
 buttonBookmarks.addEventListener('click', navigateToBookmarks)
 buttonCreate.addEventListener('click', navigateToCreate)
 buttonSettings.addEventListener('click', navigateToSettings)
-
 function navigateToHome() {
   changePage(homePage)
   selectButton(buttonHome)
 }
-
 function navigateToBookmarks() {
   changePage(bookmarksPage)
   selectButton(buttonBookmarks)
 }
-
 function navigateToCreate() {
   changePage(createPage)
   selectButton(buttonCreate)
 }
-
 function navigateToSettings() {
   changePage(settingsPage)
   selectButton(buttonSettings)
 }
-
 function changePage(page) {
   hideAllPages()
   page.classList.remove('hidden')
 }
-
 function selectButton(button) {
   deactivateButtons()
   button.classList.add('active')
 }
-
 function hideAllPages() {
   homePage.classList.add('hidden')
   bookmarksPage.classList.add('hidden')
@@ -53,4 +47,8 @@ function deactivateButtons() {
   buttonBookmarks.classList.remove('active')
   buttonCreate.classList.remove('active')
   buttonSettings.classList.remove('active')
+}
+
+function getElement(selector) {
+  return document.querySelector(selector)
 }
