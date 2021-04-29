5a696668900ff590d07734bbd2eea6ae3bc0b5d5
Pascal Weber <weber-pascal@web.de>
2021-04-29 14:27:44 +0200

    java

--- src/index.js
+++ src/index.js
@@ -1,11 +1,56 @@
-const form = document.querySelector('[data-form]')
-const box = document.querySelector('.box')
-const input1 = document.querySelector('[name="input1"]')
+const buttonHome = document.querySelector('.button-home')
+const buttonBookmarks = document.querySelector('.button-bookmarks')
+const buttonCreate = document.querySelector('.button-create')
+const buttonSettings = document.querySelector('.button-settings')
 
-form.addEventListener('submit', event => {
-  event.preventDefault()
-  const inputText1 = input1.value
-  box.style.background = inputText1
-  form.reset()
-  input1.focus()
+const homePage = document.querySelector('.page-home')
+const bookmarksPage = document.querySelector('.page-bookmarks')
+const createPage = document.querySelector('.page-create')
+const settingsPage = document.querySelector('.page-settings')
+
+buttonHome.addEventListener('click', () => {
+  changePage(homePage)
+
+  deactivateButtons()
+  buttonHome.classList.add('active')
 })
+
+buttonBookmarks.addEventListener('click', () => {
+  changePage(bookmarksPage)
+
+  deactivateButtons()
+  buttonBookmarks.classList.add('active')
+})
+
+buttonCreate.addEventListener('click', () => {
+  changePage(createPage)
+
+  deactivateButtons()
+  buttonCreate.classList.add('active')
+})
+
+buttonSettings.addEventListener('click', () => {
+  changePage(settingsPage)
+
+  deactivateButtons()
+  buttonSettings.classList.add('active')
+})
+
+function changePage(page) {
+  hideAllPages()
+  page.classList.remove('hidden')
+}
+
+function hideAllPages() {
+  homePage.classList.add('hidden')
+  bookmarksPage.classList.add('hidden')
+  createPage.classList.add('hidden')
+  settingsPage.classList.add('hidden')
+}
+
+function deactivateButtons() {
+  buttonHome.classList.remove('active')
+  buttonBookmarks.classList.remove('active')
+  buttonCreate.classList.remove('active')
+  buttonSettings.classList.remove('active')
+}
